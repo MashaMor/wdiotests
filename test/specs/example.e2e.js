3fdf6137dad8b6aec45f1afc6e786e6631726277
mariiamoria <mariia.moria@gmail.com>
2021-03-17 18:15:12 -0700

    first commit

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,15 @@
+import LoginPage from  '../../(./test/pageobjects/login.page';
+import SecurePage from '../../(./test/pageobjects/secure.page';
+
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        LoginPage.open();
+
+        LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        expect(SecurePage.flashAlert).toBeExisting();
+        expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
+

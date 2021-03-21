8f5f8447cdd74f8d4cea42933fea168232950b92
Mariia Moria <mariia.moria@gmail.com>
2021-03-20 21:26:18 -0700

    Fourth commit

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -4,6 +4,11 @@ class LoginPage extends Page {
     get inputUsername () { return $('#normal_login_email'); }
     get inputPassword () { return $('#normal_login_password'); }
     get buttonSubmit () { return $('.login-form-button'); }
+    get errorToast () { return $('.ant-notification-notice-message'); }
+
+    open () {
+        return super.open('/');
+    }
 
     setLogin (email) {
         this.inputUsername.setValue(email);
@@ -17,8 +22,12 @@ class LoginPage extends Page {
         this.buttonSubmit.click();
     }
 
-    open () {
-        return super.open('/');
+    submitButtonIsDisabled() {
+        expect(this.buttonSubmit).toBeDisabled();
+    }
+
+    errorToastAppeared() {
+        expect(this.errorToast).toBeDisplayed();
     }
 }
 
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,13 +2,32 @@ import LoginPage from '../pageobjects/login.page';
 import ProfilePage from '../pageobjects/profile.page';
 
 describe('Authorization', () => {
-    it('user logs in with valid data', () => {
+    beforeEach(() => {
         LoginPage.open();
+    })
+
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()');
+    });
+
+    it('user logs in with valid data', () => {
         LoginPage.setLogin('bilota8274@990ys.com');
         LoginPage.setPassword('1234567890Alice');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
     });
+
+    it('submit button is disabled if login and password are absent', () => {
+        LoginPage.submitButtonIsDisabled();
+    });
+
+    it('fails if invalid data provided', () => {
+        LoginPage.setLogin('example@example.com');
+        LoginPage.setPassword('123456');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
+    });
+
 });
 
 
